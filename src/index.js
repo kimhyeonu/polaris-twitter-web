c0073891108663e878c5930dd9983447affc7cd9
kimhyeonu <polaris@byeolmuri.com>
2021-07-15 13:37:30 +0900

    Firebase 구성 | .env 구성 정보 은닉

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from './firebase';
 
 import App from './App';
+
+// TODO: 추후 제거 예정
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
