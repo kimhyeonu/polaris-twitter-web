44e3c1110904f43d9a2f94f92e530e8b6e6c9ba4
kimhyeonu <polaris@byeolmuri.com>
2021-07-15 17:54:48 +0900

    접속 여부(isSignedIn)에 따른 홈, 인증 페이지 라우팅

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
+// 루트 라우트 목록
+import Home from '../routes/Home';
+import Auth from '../routes/Auth';
+
 const AppRouter = () => {
+  // isSignedIn이 true이면 접속 상태 →  홈 페이지
+  // isSignedIn이 false이면 비접속 상태 → 인증 페이지
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <Router>
       <Switch>
-        <Route />
+        {isSignedIn ? (
+          <Route exact path="/">
+            <Home />
+          </Route>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
       </Switch>
     </Router>
   );
