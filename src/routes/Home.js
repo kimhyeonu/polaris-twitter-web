6275b2edb9d7c8d55f334aae0af92d7e2d651af4
kimhyeonu <polaris@byeolmuri.com>
2021-07-16 21:52:51 +0900

    트윗(tweet) 생성 및 조회 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,59 @@
-const Home = () => <span>Home</span>;
+import { useState, useEffect } from 'react';
+
+import { firestoreService } from 'firebaseApp';
+
+const Home = () => {
+  const [tweet, setTweet] = useState('');
+  const [tweets, setTweets] = useState([]);
+
+  const getTweets = async () => {
+    const tweets = await firestoreService.collection('tweets').get();
+
+    tweets.forEach((document) =>
+      setTweets((prev) => [document.data(), ...prev])
+    );
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await firestoreService.collection('tweets').add({
+      text: tweet,
+      createdAt: Date.now(),
+    });
+
+    setTweet('');
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+
+    const {
+      target: { value },
+    } = event;
+
+    setTweet(value);
+  };
+
+  useEffect(() => {
+    getTweets();
+  }, []);
+
+  console.log(tweets);
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={tweet}
+        onChange={onChange}
+        type="text"
+        placeholder="100자 이내로 입력해 주세요."
+        maxLength={100}
+      />
+
+      <input type="submit" value="등록" />
+    </form>
+  );
+};
 
 export default Home;
