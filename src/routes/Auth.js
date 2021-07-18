dce46560a6fb081afeea4a559735a9e1f066183a
kimhyeonu <polaris@byeolmuri.com>
2021-07-19 00:31:44 +0900

    [리팩토링] Auth 컴포넌트의 이메일 접속 관련 폼을 AuthForm 컴포넌트로 분리

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,49 +1,7 @@
-import { useState } from 'react';
-
 import { authService, firebaseInstance } from 'firebaseApp';
+import AuthForm from 'components/AuthForm';
 
 const Auth = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  // * 신규 계정 → true
-  // * 기존 계정 -> false
-  const [isNewAccount, setIsNewAccount] = useState(true);
-
-  const [error, setError] = useState('');
-
-  const onChange = (event) => {
-    const {
-      target: { name, value },
-    } = event;
-
-    if (name === 'email') {
-      setEmail(value);
-    } else if (name === 'password') {
-      setPassword(value);
-    }
-  };
-
-  // * 비동기 처리
-  const onSubmit = async (event) => {
-    // * 페이지 새로고침 방지 → React 상태 손실 방지
-    event.preventDefault();
-
-    try {
-      if (isNewAccount) {
-        // * 신규 계정일 경우 계정을 생성한다.
-        await authService.createUserWithEmailAndPassword(email, password);
-      } else {
-        // * 기존 계정일 경우 서비스에 접속한다.
-        await authService.signInWithEmailAndPassword(email, password);
-      }
-    } catch (err) {
-      setError(err.message);
-    }
-  };
-
-  const toggleAccount = () => setIsNewAccount((prev) => !prev);
-
   // * 비동기 처리
   const onSocialClick = async (event) => {
     const {
@@ -62,36 +20,7 @@ const Auth = () => {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          name="email"
-          type="email"
-          placeholder="이메일"
-          value={email}
-          required
-          onChange={onChange}
-        />
-
-        <input
-          name="password"
-          type="password"
-          placeholder="패스워드"
-          value={password}
-          required
-          onChange={onChange}
-        />
-
-        <input
-          type="submit"
-          value={isNewAccount ? '계정 생성' : '서비스 접속'}
-        />
-
-        {error}
-      </form>
-
-      <span onClick={toggleAccount}>
-        {isNewAccount ? '서비스 접속' : '계정 생성'}
-      </span>
+      <AuthForm />
 
       <div>
         <button onClick={onSocialClick} name="google">
